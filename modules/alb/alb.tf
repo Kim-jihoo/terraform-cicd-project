ab9045be84a20e60a40bc4302e0bef021e4c6f30
jihoo <jihoo1106@ewhain.net>
2025-03-26 11:40:26 +0900

    alb log용 S3 코드 추가(alb.tf

--- modules/alb/alb.tf
+++ modules/alb/alb.tf
@@ -1,4 +1,64 @@
 ##ALB
+
+#alb log S3 버킷 추가
+# ALB 로그 저장용 S3 버킷
+resource "aws_s3_bucket" "alb_logs" {
+  bucket = "jihoo-alb-access-logs"
+  force_destroy = true
+
+  tags = merge(tomap({
+    Name = "jihoo-alb-access-logs"
+  }), var.tags)
+}
+
+resource "aws_s3_bucket_ownership_controls" "alb_logs" {
+  bucket = aws_s3_bucket.alb_logs.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "alb_logs" {
+  bucket = aws_s3_bucket.alb_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_acl" "alb_logs" {
+  depends_on = [aws_s3_bucket_ownership_controls.alb_logs]
+  bucket     = aws_s3_bucket.alb_logs.id
+  acl        = "private"
+}
+
+resource "aws_s3_bucket_policy" "alb_logs" {
+  bucket = aws_s3_bucket.alb_logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid = "AWSALBLogDeliveryWrite",
+        Effect = "Allow",
+        Principal = {
+          Service = "logdelivery.elasticloadbalancing.amazonaws.com"
+        },
+        Action = "s3:PutObject",
+        Resource = "${aws_s3_bucket.alb_logs.arn}/*",
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
+
+
 resource "aws_lb" "alb" { #alb 생성
   name               = "aws-alb-${var.stage}-${var.servicename}"
   internal           = var.internal
@@ -9,6 +69,7 @@ resource "aws_lb" "alb" { #alb 생성
   enable_deletion_protection = true
 
   idle_timeout = var.idle_timeout
+  depends_on = [aws_s3_bucket.alb_logs]
 
   access_logs {
     bucket  = var.aws_s3_lb_logs_name
