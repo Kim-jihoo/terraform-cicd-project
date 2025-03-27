3c31d4c7ffe20fdc34b3f59b1ef01c51fc9d8d3a
jihoo <jihoo1106@ewhain.net>
2025-03-27 17:59:32 +0900

    terraform_prod 생성

---
+++ terraform_prod/main.tf
@@ -0,0 +1,199 @@
+terraform {
+ required_version = ">= 1.0.0, < 2.0.0"
+
+  backend "s3" {
+    bucket = "jihoo-terraform-state"
+    key  = "prod/terraform/terraform.tfstate"
+    region = "ap-northeast-2"
+    encrypt = true
+    dynamodb_table = "jihoo-terraform-state"
+  }
+}
+
+##Sharedservic resources
+module "vpc" {
+  source              = "../modules/vpc"
+
+  stage       = var.stage
+  servicename = var.servicename
+  tags        = var.tags
+  # region     = var.region
+  # kms_arn = var.s3_kms_key_id
+
+  vpc_ip_range = var.vpc_ip_range
+  az           = var.az
+
+  subnet_public_az1 = var.subnet_public_az1
+  subnet_public_az2 = var.subnet_public_az2
+  subnet_service_az1 = var.subnet_service_az1
+  subnet_service_az2 = var.subnet_service_az2
+  subnet_db_az1  = var.subnet_db_az1
+  subnet_db_az2  = var.subnet_db_az2
+ 
+
+ 
+  ##SecurityGroup
+  #sg_allow_comm_list = concat(var.ext_sg_allow_list, ["${module.vpc.nat_ip}/32", var.vpc_ip_range])
+
+  ##TGW
+  #create_tgw = var.create_tgw
+  #tgw_sharing_accounts = var.tgw_sharing_accounts
+#   ext_vpc_route = var.ext_vpc_route
+  #security_attachments = var.security_attachments
+  #auto_accept_shared_attachments = true
+  #security_attachments_propagation = merge(var.security_attachments_propagation, var.security_attachments)
+}
+
+#ALB
+module "alb" {
+  source = "../modules/alb"
+
+  stage       = var.stage
+  servicename = var.servicename
+  tags        = var.tags
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = [module.vpc.service-az1.id, module.vpc.service-az2.id]
+
+  sg_allow_comm_list   = var.sg_allow_comm_list
+  aws_s3_lb_logs_name  = var.aws_s3_lb_logs_name
+  idle_timeout         = var.idle_timeout
+  certificate_arn      = var.certificate_arn
+  port                 = var.port
+  domain               = var.domain
+  hostzone_id          = var.hostzone_id
+  hc_path              = var.hc_path
+  hc_healthy_threshold = var.hc_healthy_threshold
+  hc_unhealthy_threshold = var.hc_unhealthy_threshold
+}
+
+# ECS
+module "ecs" {
+  source = "../modules/ecs"
+
+  stage       = var.stage
+  servicename = var.servicename
+  tags        = var.tags
+
+  cluster_name        = "ecs-cluster-${var.stage}-${var.servicename}"
+  service_name        = "ecs-service-${var.stage}-${var.servicename}"
+  container_image     = var.container_image
+  container_port      = var.container_port
+  cpu                 = var.cpu
+  memory              = var.memory
+  subnet_ids          = [module.vpc.service-az1.id, module.vpc.service-az2.id]
+  vpc_id              = module.vpc.vpc_id
+  alb_sg_id           = module.alb.sg-alb-id
+  security_group_ids  = [] # 보안 그룹 직접 생성되므로 이 필드는 사용되지 않음
+  target_group_arn    = module.alb.target-group-arn
+  execution_role_arn  = var.execution_role_arn
+  task_role_arn       = var.task_role_arn
+  desired_count       = var.desired_count
+  assign_public_ip    = false
+  #alb_listener_dependency = [module.alb.lb-listener-443]
+  alb_listener_dependency = var.certificate_arn != "" ? [module.alb.lb-listener-443[0]] : [] #certificate_arn이 비어있을 때
+
+}
+# ecs_instance
+module "ecs_instance" {
+  source = "../modules/ecs_instance"
+
+  stage                = var.stage
+  servicename          = var.servicename
+  tags                 = var.tags
+  ami_id               = var.ami_id
+  instance_type        = var.instance_type
+  subnet_ids           = [module.vpc.service-az1.id, module.vpc.service-az2.id]
+  security_group_ids = [module.ecs.sg-ecs-id]
+  instance_profile_name = "ecsInstanceRole" # 사전에 생성해둔 EC2용 IAM 역할
+  key_name             = var.key_name
+  cluster_name         = module.ecs.ecs_cluster_name
+}
+
+#RDS
+ module "rds" {
+  source       = "../modules/aurora"
+  stage        = var.stage
+  servicename  = var.servicename
+  tags         = var.tags
+  dbname       = var.rds_dbname
+
+  sg_allow_ingress_list_aurora = var.sg_allow_ingress_list_aurora
+  network_vpc_id               = module.vpc.vpc_id
+  subnet_ids                   = [module.vpc.db-az1.id, module.vpc.db-az2.id]
+  az                           = var.az
+
+  rds_instance_count = var.rds_instance_count
+  kms_key_id         = var.rds_kms_arn
+  enabled_cloudwatch_logs_exports = ["audit", "error", "general", "slowquery"]
+  sg_allow_ingress_sg_list_aurora = var.sg_allow_ingress_sg_list_aurora
+  depends_on = [module.vpc]
+}
+
+# module "jihoo-ec2" {
+#   source              = "../modules/instance"
+
+#   stage        = var.stage
+#   servicename  = "${var.servicename}"
+#   tags         = var.tags
+
+#   ami                       = var.ami
+#   instance_type             = var.instance_type
+#   ebs_size                  = var.instance_ebs_size
+#   #user_data                 = var.instance_user_data
+#   kms_key_id                = var.ebs_kms_key_id
+#   ec2-iam-role-profile-name = module.iam-service-role.ec2-iam-role-profile.name
+#   ssh_allow_comm_list       = [var.subnet_service_az1, var.subnet_service_az2]
+
+#   associate_public_ip_address = var.associate_public_ip_address
+
+#   subnet_id = module.vpc.public-az1.id
+#   vpc_id    = module.vpc.vpc_id
+#   user_data = <<-EOF
+# #!/bin/bash 
+# yum update -y 
+# yum install -y https://s3.ap-northeast-2.amazonaws.com/amazon-ssm-ap-northeast-2/latest/linux_amd64/amazon-ssm-agent.rpm
+# EOF
+#   ##SecurityGroup
+#   sg_ec2_ids = [aws_security_group.sg-ec2.id]
+#   #depends_on = [module.vpc.sg-ec2-comm, module.iam-service-role.ec2-iam-role-profile]
+# }
+
+# resource "aws_security_group" "sg-ec2" {
+#   name   = "aws-sg-${var.stage}-${var.servicename}-ec2"
+#   vpc_id = module.vpc.vpc_id
+
+#   ingress {
+#     from_port   = 443
+#     to_port     = 443
+#     protocol    = "TCP"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = ""
+#   }
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "TCP"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = ""
+#   }
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "TCP"
+#     cidr_blocks = ["0.0.0.0/0"]
+#     description = ""
+#   }
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+#   tags = merge(tomap({
+#          Name = "aws-sg-${var.stage}-${var.servicename}-ec2"}), 
+#         var.tags)
+# }
+
+
+
---
+++ terraform_prod/variable.tf
@@ -0,0 +1,226 @@
+variable "region" {
+  type = string
+  default = "ap-northeast-2"
+}
+variable "stage" {
+  type = string
+  default = "prod"
+}
+variable "servicename" {
+  type = string
+  default = "terraform-jihoo"
+}
+variable "tags" {
+  type = map(string)
+  default = {
+    "name" = "jihoo_VPC"
+  }
+}
+
+#VPC
+variable "az" {
+  type = list(any)
+  default = [ "ap-northeast-2a", "ap-northeast-2c" ]
+}
+variable "vpc_ip_range" {
+  type = string
+  default = "10.2.93.0/24"
+}
+
+variable "subnet_public_az1" {
+  type = string
+  default = "10.2.93.0/27"
+}
+variable "subnet_public_az2" {
+  type = string
+  default = "10.2.93.32/27"
+}
+variable "subnet_service_az1" {
+  type = string
+  default = "10.2.93.64/26"
+}
+variable "subnet_service_az2" {
+  type = string
+  default = "10.2.93.128/26"
+}
+variable "subnet_db_az1" {
+  type = string
+  default = "10.2.93.192/27"
+}
+variable "subnet_db_az2" {
+  type = string
+  default = "10.2.93.224/27"
+}
+# variable "create_tgw" {
+#   type = bool
+#   default = false
+# }
+# variable "ext_vpc_route" {
+#   type = any
+# }
+# variable "security_attachments" {
+#   type = any
+# }
+# variable "security_attachments_propagation" {
+#   type = any
+# }
+# variable "tgw_sharing_accounts" {
+#   type = map
+# }
+
+#ALB
+variable "aws_s3_lb_logs_name" {
+  type = string
+  default = "jihoo-alb-access-logs"
+}
+
+variable "idle_timeout" {
+  type    = string
+  default = "60"
+}
+
+variable "certificate_arn" {
+  type = string
+  default = "" # ACM에서 발급받은 인증서 ARN
+}
+
+variable "domain" {
+  type    = string
+  default = "" # ex: example.com
+}
+
+variable "hostzone_id" {
+  type    = string
+  default = "" # Route 53 호스팅존 ID
+}
+variable "port" {
+  type    = string
+  default = "80"
+}
+
+variable "hc_path" {
+  type    = string
+  default = "/"
+}
+
+variable "hc_healthy_threshold" {
+  type    = number
+  default = 5
+}
+
+variable "hc_unhealthy_threshold" {
+  type    = number
+  default = 2
+}
+variable "sg_allow_comm_list" {
+  type    = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+
+##Instance
+/* ami_id와 중복으로 제거
+variable "ami"{
+  type = string
+  default = "ami-04c596dcf23eb98d8"
+}*/
+variable "instance_type" {
+  type = string
+  default = "t2.micro"
+}
+variable "instance_ebs_size" {
+  type = number
+  default = 20
+}
+variable "instance_ebs_volume" {
+  type = string
+  default = "gp3"
+}
+
+# variable "instance_user_data" {
+#   type = string
+# }
+# variable "redis_endpoints" {
+#   type = list
+# }
+
+##RDS
+variable "rds_dbname" {
+  type = string
+  default = "jihoo"
+}
+variable "rds_instance_count" {
+  type = string
+  default = "2"
+}
+variable "sg_allow_ingress_list_aurora"{
+  type = list
+  default = ["10.2.92.64/26", "10.2.92.128/26", "10.2.92.18/32"]
+}
+variable "associate_public_ip_address" {
+  type = bool
+  default = true
+}
+
+##KMS
+variable "rds_kms_arn" {
+  type    = string
+  default = "arn:aws:kms:ap-northeast-2:762233749320:key/32436012-6187-473c-8449-3a4d90b6d73c"
+}
+variable "ebs_kms_key_id" {
+  type = string
+  default = "arn:aws:kms:ap-northeast-2:471112992234:key/43b0228d-0a06-465c-b25c-7480b07b5276"
+}
+
+# ECS
+variable "container_image" {
+  type    = string
+  default = "nginx:latest"
+}
+
+variable "execution_role_arn" {
+  type = string
+  default = "arn:aws:iam::762233749320:role/ecsTaskExecutionRole"
+}
+
+
+variable "task_role_arn" {
+  type    = string
+  default = "arn:aws:iam::762233749320:role/ecsTaskRole"
+}
+
+
+variable "container_port" {
+  type    = number
+  default = 80
+}
+
+variable "cpu" {
+  type    = number
+  default = 256
+}
+
+variable "memory" {
+  type    = number
+  default = 512
+}
+
+variable "desired_count" {
+  type    = number
+  default = 1
+}
+
+# ecs_instance
+variable "ami_id" {
+  type    = string
+  default = "ami-0a07a3809aa66dcac"
+}
+
+variable "key_name" {
+  type    = string
+  default = "" # 실제 EC2에 접근하려면 EC2 키페어 이름을 여기에 넣어야 합니다
+}
+variable "sg_allow_ingress_sg_list_aurora" {
+  type    = list(string)
+  default = []
+}
