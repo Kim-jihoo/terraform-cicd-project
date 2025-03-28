038cad7a552ee2dc0598d4d93a8abe93ecf8386c
jihoo <jihoo1106@ewhain.net>
2025-03-28 14:44:23 +0900

    중복 정의 제거

--- modules/cloudfront_static/output.tf
+++ modules/cloudfront_static/output.tf
@@ -7,9 +7,6 @@ output "cloudfront_domain_name" {
   description = "CloudFront distribution domain name"
   value       = aws_cloudfront_distribution.frontend.domain_name
 }
-output "cloudfront_domain_name" {
-  value = aws_cloudfront_distribution.frontend.domain_name
-}
 
 output "cloudfront_hosted_zone_id" {
   value = "Z2FDTNDATAQYW2" # CloudFront 고정 값 (AWS 공식)
